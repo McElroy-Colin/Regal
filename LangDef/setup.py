9d53ed217bd5232ebcd6393fcae0a5edd57a6234
Colin McElroy <158326955+McElroy-Colin@users.noreply.github.com>
2024-04-29 21:36:37 -0400

    Delete LangDef/setup.py

--- LangDef/setup.py
+++
@@ -1,6 +0,0 @@
-# Setup file to handle package imports for parent and sibling directories.
-# Running 'pip install .' in the LangDef folder will correctly import into all files.
-
-from setuptools import setup, find_packages
-
-setup(name = 'BppLanguageDefinition', version = '1.0', packages = find_packages())
